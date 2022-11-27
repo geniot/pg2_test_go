379d66c7606a19a369786765d0443e9bc4998a47
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2022-11-28 00:12:35 +0100

    Refactor constants, make png transparent

--- constants.go
+++ constants.go
@@ -2,29 +2,37 @@ package main
 
 import "github.com/veandco/go-sdl2/sdl"
 
-const GCW_BUTTON_UP = sdl.K_UP
-const GCW_BUTTON_DOWN = sdl.K_DOWN
-const GCW_BUTTON_LEFT = sdl.K_LEFT
-const GCW_BUTTON_RIGHT = sdl.K_RIGHT
+const (
+	FONT_PATH              = "media/pixelberry.ttf"
+	FONT_SIZE              = 14
+	SCREEN_WIDTH           = 320
+	SCREEN_HEIGHT          = 240
+	SECOND_SCREEN_X_OFFSET = 2500 //used for testing/debugging
 
-const GCW_BUTTON_A = sdl.K_LCTRL
-const GCW_BUTTON_B = sdl.K_LALT
-const GCW_BUTTON_X = sdl.K_SPACE
-const GCW_BUTTON_Y = sdl.K_LSHIFT
+	GCW_BUTTON_UP    = sdl.K_UP
+	GCW_BUTTON_DOWN  = sdl.K_DOWN
+	GCW_BUTTON_LEFT  = sdl.K_LEFT
+	GCW_BUTTON_RIGHT = sdl.K_RIGHT
 
-const GCW_BUTTON_L1 = sdl.K_TAB
-const GCW_BUTTON_R1 = sdl.K_BACKSPACE
+	GCW_BUTTON_A = sdl.K_LCTRL
+	GCW_BUTTON_B = sdl.K_LALT
+	GCW_BUTTON_X = sdl.K_SPACE
+	GCW_BUTTON_Y = sdl.K_LSHIFT
 
-const GCW_BUTTON_L2 = sdl.K_RSHIFT
-const GCW_BUTTON_R2 = sdl.K_RALT
+	GCW_BUTTON_L1 = sdl.K_TAB
+	GCW_BUTTON_R1 = sdl.K_BACKSPACE
 
-//const GCW_BUTTON_L2 = sdl.K_PAGEUP
-//const GCW_BUTTON_R2 = sdl.K_PAGEDOWN
+	GCW_BUTTON_L2 = sdl.K_RSHIFT
+	GCW_BUTTON_R2 = sdl.K_RALT
 
-const GCW_BUTTON_SELECT = sdl.K_ESCAPE
-const GCW_BUTTON_START = sdl.K_RETURN
-const GCW_BUTTON_MENU = sdl.K_RCTRL
+	//GCW_BUTTON_L2 = sdl.K_PAGEUP
+	//GCW_BUTTON_R2 = sdl.K_PAGEDOWN
 
-const GCW_BUTTON_L3 = sdl.K_KP_DIVIDE
-const GCW_BUTTON_R3 = sdl.K_KP_PERIOD
-const GCW_BUTTON_POWER = sdl.K_HOME
+	GCW_BUTTON_SELECT = sdl.K_ESCAPE
+	GCW_BUTTON_START  = sdl.K_RETURN
+	GCW_BUTTON_MENU   = sdl.K_RCTRL
+
+	GCW_BUTTON_L3    = sdl.K_KP_DIVIDE
+	GCW_BUTTON_R3    = sdl.K_KP_PERIOD
+	GCW_BUTTON_POWER = sdl.K_HOME
+)
--- pg2test.go
+++ pg2test.go
@@ -2,17 +2,9 @@ package main
 
 import (
 	mapset "github.com/deckarep/golang-set/v2"
+	"github.com/veandco/go-sdl2/img"
 	"github.com/veandco/go-sdl2/sdl"
 	"github.com/veandco/go-sdl2/ttf"
-	"strconv"
-)
-
-const (
-	fontPath           = "media/pixelberry.ttf"
-	fontSize           = 14
-	width              = 320
-	height             = 240
-	secondScreenOffset = 2500 //used for testing/debugging
 )
 
 func main() {
@@ -23,6 +15,7 @@ func main() {
 	var text *sdl.Surface
 	var err error
 	var pressedKeysCodes = mapset.NewSet[sdl.Keycode]()
+	var pngImage *sdl.Surface
 
 	if err = ttf.Init(); err != nil {
 		return
@@ -37,10 +30,10 @@ func main() {
 	numVideoDisplay, err := sdl.GetNumVideoDisplays()
 
 	window, err = sdl.CreateWindow(
-		"test",
-		int32(If(numVideoDisplay > 1, secondScreenOffset, sdl.WINDOWPOS_UNDEFINED)),
+		"pg2_test_go",
+		int32(If(numVideoDisplay > 1, SECOND_SCREEN_X_OFFSET, sdl.WINDOWPOS_UNDEFINED)),
 		sdl.WINDOWPOS_UNDEFINED,
-		width, height,
+		SCREEN_WIDTH, SCREEN_HEIGHT,
 		sdl.WINDOW_SHOWN|sdl.WINDOW_BORDERLESS)
 
 	if err != nil {
@@ -53,13 +46,25 @@ func main() {
 		panic(err)
 	}
 
-	surface.FillRect(nil, 0xffffffff)
+	surface.FillRect(nil, sdl.MapRGB(surface.Format, 16, 16, 16))
 
 	// Load the font for our text
-	if font, err = ttf.OpenFont(fontPath, fontSize); err != nil {
+	if font, err = ttf.OpenFont(FONT_PATH, FONT_SIZE); err != nil {
 		panic(err)
 	}
 	defer font.Close()
+
+	if pngImage, err = img.Load("media/pg2_back.png"); err != nil {
+		panic(err)
+	}
+	defer pngImage.Free()
+
+	//var colorKey, _ = pngImage.GetColorKey()
+	//var surf, _ = sdl.CreateRGBSurface(colorKey, pngImage.W, pngImage.H, 16, 0, 0, 0, 0)
+	//pngImage.Blit(nil, surf, nil)
+	pngImage.Blit(nil, surface, &sdl.Rect{X: 90, Y: 50, W: pngImage.W, H: pngImage.H})
+	//surf.SetColorKey(true, sdl.MapRGB(surface.Format, 255, 0, 255))
+	//defer surf.Free()
 
 	// Create a red text with the font
 	if text, err = font.RenderUTF8Blended("Hello, World!", sdl.Color{R: 255, G: 0, B: 0, A: 255}); err != nil {
@@ -68,7 +73,7 @@ func main() {
 	defer text.Free()
 
 	// Draw the text around the center of the window
-	if err = text.Blit(nil, surface, &sdl.Rect{X: width/2 - text.W/2, Y: height/2 - text.H/2, W: 0, H: 0}); err != nil {
+	if err = text.Blit(nil, surface, &sdl.Rect{X: SCREEN_WIDTH/2 - text.W/2, Y: SCREEN_HEIGHT/2 - text.H/2, W: 0, H: 0}); err != nil {
 		return
 	}
 
@@ -78,33 +83,35 @@ func main() {
 	for running {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch t := event.(type) {
-			case *sdl.QuitEvent:
-				running = false
 			case *sdl.KeyboardEvent:
 				keyCode := t.Keysym.Sym
-				if t.State == sdl.RELEASED {
-					pressedKeysCodes.Remove(keyCode)
-				} else if t.State == sdl.PRESSED {
+				if t.State == sdl.PRESSED {
 					pressedKeysCodes.Add(keyCode)
+				} else { // if t.State == sdl.RELEASED {
+					pressedKeysCodes.Remove(keyCode)
 				}
-
-				rect := sdl.Rect{0, 0, width, height}
-				surface.FillRect(&rect, 0xffffffff)
-				var txt = strconv.Itoa(int(keyCode)) + ":" + string(keyCode)
-				text, err = font.RenderUTF8Blended(txt, sdl.Color{R: 255, G: 0, B: 0, A: 255})
-				text.Blit(nil, surface, &sdl.Rect{X: width/2 - text.W/2, Y: height/2 - text.H/2, W: 0, H: 0})
-				window.UpdateSurface()
-
 				if pressedKeysCodes.Contains(sdl.K_q) ||
 					(pressedKeysCodes.Contains(GCW_BUTTON_L1) && pressedKeysCodes.Contains(GCW_BUTTON_START)) {
 					running = false
 				}
+				redraw()
 			}
 		}
 
 		//sdl.Delay(16)
 	}
 
+}
+
+func redraw() {
+	//rect := sdl.Rect{0, 0, SCREEN_WIDTH, SCREEN_HEIGHT}
+	//surface.FillRect(&rect, sdl.MapRGB(surface.Format, 16, 16, 16))
+	//
+	//var txt = strconv.Itoa(int(keyCode)) + ":" + string(keyCode)
+	//text, err = font.RenderUTF8Blended(txt, sdl.Color{R: 255, G: 0, B: 0, A: 255})
+	//text.Blit(nil, surface, &sdl.Rect{X: SCREEN_WIDTH/2 - text.W/2, Y: SCREEN_HEIGHT/2 - text.H/2, W: 0, H: 0})
+	//
+	//window.UpdateSurface()
 }
 
 func If[T any](cond bool, vTrue, vFalse T) T {
